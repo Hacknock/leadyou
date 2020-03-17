379c359efcb76f6cc8fa92c3dab0bbdaa4c29606
Akira Kashihara <akira.kashihara@hotmail.com>
2020-03-17 13:55:53 +0900

    Add node.js code to route css,javascript,image files.

--- index.js
+++ index.js
@@ -8,18 +8,45 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-var server = http.createServer(app);
+let server = http.createServer(app);
 const mongo = require('mongodb');   //momgodb define
 const MongoClient = mongo.MongoClient;  //use mongo client
 
 const port=3000;
 
 server.listen(port, ()=>{
-    console.log('listen port 3000')
+    console.log('listen port 3000 #00')
 });
 
-var jsonInsert = {
-    idGithub: '',
+const connectOption = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+
+function insertData(jsonData) {
+    MongoClient.connect('mongodb://127.0.0.1:27017', connectOption,(err, client) =>{
+        if(err) {
+            console.log("Error occurred #01");
+            throw err;
+        } else {
+            console.log("Database connection is established on insert data process. #02");
+            const db = client.db('pullreqme');  //Get pullreqme database
+            db.collection('dbSearch', (err, collection)=>{
+               collection.find({idGithub: jsonData.idGithub}).toArray((err, items) =>{
+                   console.log(items.length);
+                   if(items.length == 0) {
+                        console.log()
+                   } else {
+
+                   }
+               });
+            });
+        }
+    });
+}
+
+let jsonInsert = {
+    idGithub: 'hai',
     nameUser: '',
     desc: '',
     linkFB: '',
@@ -40,3 +67,28 @@ var jsonInsert = {
     distr: '',
     namePub: ''
 };
+
+insertData(jsonInsert);
+
+app.get('/src/css/*', (req, res) => {
+    fs.readFile('./public/css/' + req.params[0], 'utf-8', (err, data) => {
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+        res.write(data);
+        res.end();
+    });
+});
+
+app.get('/src/js/*', (req, res) => {
+    fs.readFile('./public/js/' + req.params[0], 'utf-8', (err, data) => {
+        res.writeHead(200, { 'Content-Type': 'text/javascript' });
+        res.write(data);
+        res.end();
+    });
+});
+
+//img file rooting
+app.get('/src/img/*', function(req, res){
+    console.log(req.params[0]);
+    var buf = fs.readFileSync('./public/img/'+ req.params[0]);
+    res.send(buf, { 'Content-Type': 'image' }, 200);
+});
