70357ae2c49613a3f073e2db46c039675aef1365
KASHIHARAAkira <akira.kashihara@hotmail.com>
2020-06-28 16:32:11 +0900

    Implement oneline input form.

---
+++ public/js/customElement/wrap-oneline-field.js
@@ -0,0 +1,43 @@
+class WrapOnelineField extends HTMLElement {
+    constructor() {
+        // Always call super first in constructor
+        super();
+
+        //Create a shadow root
+        const shadow = this.attachShadow({ mode: 'open' });
+
+        // Create sections
+        const wrapper = document.createElement('section');
+
+        // Create title holder
+        const subtitle = document.createElement('h2');
+        const titleSub = this.getAttribute('nameTitle');
+        subtitle.textContent = titleSub;
+
+        // Create description holder
+        const description = document.createElement('p');
+        const descShort = this.getAttribute('descShort');
+        description.textContent = descShort;
+
+        // Create form 
+        const formF = document.createElement('form');
+        const inputF = document.createElement('input');
+        inputF.setAttribute('type', 'text');
+        inputF.setAttribute('maxlength', 140);
+        formF.appendChild(inputF);
+
+        // Create some CSS to apply to the shadow dom
+        const style = document.createElement('style');
+        console.log(style.isConnected);
+        style.textContent = ``;
+
+        // Append Child
+        shadow.appendChild(style);
+        shadow.appendChild(wrapper);
+        wrapper.appendChild(subtitle);
+        wrapper.appendChild(description);
+        wrapper.appendChild(formF);
+    }
+}
+// Define the new element
+customElements.define('wrap-oneline-field', WrapOnelineField);
