5393249c75d9e117c7c81b8fbcc368e5a413c478
KASHIHARAAkira <akira.kashihara@hotmail.com>
2020-07-01 22:27:38 +0900

    Update title with changing attribute of wrap-multiline-field

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,44 @@
 // Control general acrivity on index.html
+
+// Get setting.json which describe the layout of input form
+var myHeaders = new Headers();
+
+var myInit = {
+    method: 'GET',
+    headers: myHeaders,
+    mode: 'cors',
+    cache: 'default'
+};
+
+var myRequest = new Request('/src/json/setting.json', myInit);
+
+fetch(myRequest).then(function (response) {
+    var contentType = response.headers.get("content-type");
+    if (contentType && contentType.includes("application/json")) {
+        return response.json();
+    }
+    throw new TypeError("Oops, we haven't got JSON!");
+})
+    .then(function (json) {
+        console.log(json.readme_sections.length);
+        generateForm(json, 0);
+    })
+    .catch(function (error) { console.log(error); });
+
+// define root element which hold all custome elements.
+var rootEle = document.getElementById('areaForm');
+
+// function which generate the form user input information.
+function generateForm(jsonSetting, numArray) {
+    console.log(numArray);
+    if (jsonSetting.readme_sections.length > numArray) {
+        var childElement = document.createElement(jsonSetting.readme_sections[numArray].component);
+        childElement.setAttributeNS(null, 'nameTitle', jsonSetting.readme_sections[numArray].title);
+        rootEle.appendChild(childElement);
+        console.log(jsonSetting.readme_sections[numArray]);
+        generateForm(jsonSetting, ++numArray);
+    } else {
+        console.log("Finish read read_sections");
+    }
+}
+
