14250ab37475c053907fdfbc62069873363fed6b
KASHIHARAAkira <akira.kashihara@hotmail.com>
2020-07-03 23:17:09 +0900

    Delete or comment out not necessary console.log.

--- public/js/index.js
+++ public/js/index.js
@@ -20,7 +20,6 @@ fetch(myRequest).then(function (response) {
     throw new TypeError("Oops, we haven't got JSON!");
 })
     .then(function (json) {
-        console.log(json.sections.length);
         generateForm(json, 0);
     })
     .catch(function (error) { console.log(error); });
@@ -30,16 +29,15 @@ var rootEle = document.getElementById('areaForm');
 
 // function which generate the form user input information.
 function generateForm(jsonSetting, numArray) {
-    console.log(numArray);
     if (jsonSetting.sections.length > numArray) {
         var childElement = document.createElement(jsonSetting.sections[numArray].component);
         childElement.setAttributeNS(null, 'nameTitle', jsonSetting.sections[numArray].title);
         childElement.setAttributeNS(null, 'descShort', jsonSetting.sections[numArray].description);
         childElement.setAttributeNS(null, 'hiddenTitle', jsonSetting.sections[numArray].hidden_title);
         childElement.setAttributeNS(null, 'class', 'infoBox');
+        childElement.setAttributeNS(null, 'id', convertToId(jsonSetting.sections[numArray].title));
 
         rootEle.appendChild(childElement);
-        console.log(jsonSetting.sections[numArray]);
         generateForm(jsonSetting, ++numArray);
     } else {
         console.log("Finish read read_sections");
@@ -48,10 +46,15 @@ function generateForm(jsonSetting, numArray) {
 
 document.getElementById('sub').addEventListener('click', function () {
     var eleS2 = document.getElementsByClassName('infoBox'); //Extract all custom element
-
-    console.log(eleS2[1].shadowRoot.querySelectorAll('.field')[0].checked)
+    console.log(eleS2)
+    // console.log(eleS2[1].shadowRoot.querySelectorAll('.field')[0].checked)
     // console.log(eleS2[0].shadowRoot.getElementById('field').value);
     // console.log(eleS2[0].shadowRoot.querySelector('.field').value);
 
 })
 
+function convertToId(title) {
+    // console.log(title)
+    console.log(title.replace(' ', '_'));
+    return title.replace(' ', '_');
+}
