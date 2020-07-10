62f828f7633dac143ee6792ae5a59ccc6a1b439e
Kyome22 <kyomesuke@gmail.com>
2020-07-11 01:18:10 +0900

    scriptからgetValuesできるようにした

--- public/js/customScript/getContributers.js
+++
@@ -1,9 +0,0 @@
-module.exports.getValues = (repoUrl, authToken, secretToken) => {
-  if (repoUrl.indexOf("https://github.com/") === 0) {
-    // console.log("nyao");
-    return { title: "Hello", values: ["Worlds"] };
-  } else {
-    console.log("This url is not available.");
-  }
-  return { title: "Hello", values: ["Worlds"] };
-};
