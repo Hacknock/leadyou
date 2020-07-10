0d411905dfa277c569b363f7089ef79503ebbee4
KASHIHARAAkira <akira.kashihara@hotmail.com>
2020-07-10 21:08:51 +0900

    Modified element order of wrap-upload-files.

--- public/js/customElement/wrap-upload-file.js
+++ public/js/customElement/wrap-upload-file.js
@@ -1,65 +1,62 @@
 class WrapUploadFile extends HTMLElement {
-    constructor() {
-        // Always call super first in constructor
-        super();
+  constructor() {
+    // Always call super first in constructor
+    super();
 
-        //Create a shadow root
-        const shadow = this.attachShadow({ mode: 'open' });
+    //Create a shadow root
+    const shadow = this.attachShadow({ mode: "open" });
 
-        // Create sections
-        const wrapper = document.createElement('section');
+    // Create title holder
+    const subtitle = document.createElement("h2");
+    subtitle.setAttribute("class", "subTitle");
+    const titleSub = this.getAttribute("nameTitle");
+    subtitle.textContent = titleSub;
 
-        // Create title holder
-        const subtitle = document.createElement('h2');
-        subtitle.setAttribute('class', 'subTitle');
-        const titleSub = this.getAttribute('nameTitle');
-        subtitle.textContent = titleSub;
+    // Create description holder
+    const description = document.createElement("p");
+    description.setAttribute("class", "shortDescription");
+    const descShort = this.getAttribute("descShort");
+    description.textContent = descShort;
 
-        // Create description holder
-        const description = document.createElement('p');
-        description.setAttribute('class', 'shortDescription')
-        const descShort = this.getAttribute('descShort');
-        description.textContent = descShort;
+    // Create form
+    const upFile = document.createElement("input");
+    const descFile = document.createElement("input");
 
-        // Create form 
-        const upFile = document.createElement('input');
-        const descFile = document.createElement('input');
+    upFile.setAttribute("type", "file");
+    upFile.setAttribute("name", "fig");
+    upFile.setAttribute("class", "field");
 
-        upFile.setAttribute('type', 'file');
-        upFile.setAttribute('name', 'fig');
-        upFile.setAttribute('class', 'field');
+    descFile.setAttribute("type", "text");
+    descFile.setAttribute("class", "field");
 
-        descFile.setAttribute('type', 'text');
-        descFile.setAttribute('class', 'field');
+    // Create some CSS to apply to the shadow dom
+    const style = document.createElement("style");
+    // console.log(style.isConnected);
+    style.textContent = ``;
 
-        wrapper.appendChild(upFile);
-        wrapper.appendChild(descFile);
+    // Append Child
+    shadow.appendChild(style);
+    shadow.appendChild(subtitle);
+    shadow.appendChild(description);
+    shadow.appendChild(upFile);
+    shadow.appendChild(descFile);
+  }
 
-        // Create some CSS to apply to the shadow dom
-        const style = document.createElement('style');
-        // console.log(style.isConnected);
-        style.textContent = ``;
+  static get observedAttributes() {
+    return ["nameTitle", "descShort"];
+  }
 
-        // Append Child
-        shadow.appendChild(style);
-        shadow.appendChild(wrapper);
-        wrapper.appendChild(subtitle);
-        wrapper.appendChild(description);
+  attributeChangedCallback(attr, oldVal, newVal) {
+    // console.log('my-el attribute changed', attr);
+    // console.log('new value is ', newVal);
+    if (attr === "nameTitle") {
+      // Create title holder
+      this.shadowRoot.querySelector(".subTitle").textContent = newVal;
+    } else if (attr === "descShort") {
+      // Create description holder
+      this.shadowRoot.querySelector(".shortDescription").textContent = newVal;
     }
-
-    static get observedAttributes() { return ["nameTitle", "descShort"]; }
-
-    attributeChangedCallback(attr, oldVal, newVal) {
-        // console.log('my-el attribute changed', attr);
-        // console.log('new value is ', newVal);
-        if (attr === 'nameTitle') {
-            // Create title holder
-            this.shadowRoot.querySelector('.subTitle').textContent = newVal;
-        } else if (attr === 'descShort') {
-            // Create description holder
-            this.shadowRoot.querySelector('.shortDescription').textContent = newVal;
-        }
-    }
+  }
 }
 // Define the new element
-customElements.define('wrap-upload-file', WrapUploadFile);
+customElements.define("wrap-upload-file", WrapUploadFile);
