924130fc8e28d6c8485693f79ceb2eaf6c210a36
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2020-07-15 23:31:39 +0900

    Page transition (#52)
    * ジャンプする
    
    * オートフィルのjsonもらってくるところまで完了

--- public/js/form.js
+++ public/js/form.js
@@ -57,6 +57,17 @@ const inspectContentsJson = (contents) => {
   }
 };
 
+const inspectAutoFillJson = (contents) => {
+  for (const section of contents) {
+    if (!("title" in section && "values" in section)) {
+      throw new Error(`Section:${section.title}, contents.json is broken.`);
+    }
+    if (section.values.length == 0) {
+      throw new Error(`Section:${section.title}, contents.json is broken.`);
+    }
+  }
+};
+
 // Get template.json which describe the layout of input form
 const getJson = async (url, inspector) => {
   const options = {
@@ -194,6 +205,56 @@ const generateReadme = (template, contents) => {
   return text;
 };
 
+const inspectRequired = (eleList, referNum) => {
+  let returnNum = 0;
+  if (eleList.length > referNum) {
+    // console.log(eleList[referNum].shadowRoot.querySelector("h2").textContent);
+    console.log(eleList[referNum].getAttribute("required"));
+    if (eleList[referNum].getAttribute("required") === "true") {
+      // console.log(eleList[referNum].shadowRoot.querySelector("h2").textContent);
+      if (
+        eleList[referNum].shadowRoot
+          .querySelector(".field")
+          .querySelector(".column").value === ""
+      ) {
+        eleList[referNum].setAttribute("alert", "true");
+        returnNum = -1 + inspectRequired(eleList, ++referNum);
+      } else {
+        eleList[referNum].setAttribute("alert", "false");
+        returnNum = 0 + inspectRequired(eleList, ++referNum);
+      }
+    } else {
+      returnNum = 0 + inspectRequired(eleList, ++referNum);
+    }
+  }
+  return returnNum;
+};
+
+const getQueryStringParams = (query) => {
+  const hoge = /^[?#]/.test(query) ? query.slice(1) : query;
+  return hoge.split("&").reduce((params, param) => {
+    let [key, value] = param.split("=");
+    params[key] = value ? decodeURIComponent(value.replace(/\+/g, " ")) : "";
+    return params;
+  }, {});
+};
+
+const autoFill = () => {
+  const params = getQueryStringParams(window.location.search);
+  console.log(params);
+  if (params.autofill !== "true") return;
+  const url = `/getvalues?owner=${params.owner}&repo=${params.repo}&authToken=hello&secretToken=world`;
+  getJson(url, inspectAutoFillJson)
+    .then((json) => {
+      // auto fill されたcontents.jsonだよ。
+      console.log(json);
+      // ここでオートフィルの処理が必要。
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
 // call at loaded
 getJson("/src/json/template.json", inspectTemplateJson)
   .then((json) => {
@@ -211,6 +272,8 @@ getJson("/src/json/sample_contents.json", inspectContentsJson)
   .catch((error) => {
     console.error(error);
   });
+
+autoFill();
 
 // Submitボタンを押した時の処理
 document.getElementById("submit").addEventListener("click", () => {
@@ -245,28 +308,3 @@ document.getElementById("submit").addEventListener("click", () => {
   //   console.error(error);
   // }
 });
-
-const inspectRequired = (eleList, referNum) => {
-  let returnNum = 0;
-  if (eleList.length > referNum) {
-    // console.log(eleList[referNum].shadowRoot.querySelector("h2").textContent);
-    console.log(eleList[referNum].getAttribute("required"));
-    if (eleList[referNum].getAttribute("required") === "true") {
-      // console.log(eleList[referNum].shadowRoot.querySelector("h2").textContent);
-      if (
-        eleList[referNum].shadowRoot
-          .querySelector(".field")
-          .querySelector(".column").value === ""
-      ) {
-        eleList[referNum].setAttribute("alert", "true");
-        returnNum = -1 + inspectRequired(eleList, ++referNum);
-      } else {
-        eleList[referNum].setAttribute("alert", "false");
-        returnNum = 0 + inspectRequired(eleList, ++referNum);
-      }
-    } else {
-      returnNum = 0 + inspectRequired(eleList, ++referNum);
-    }
-  }
-  return returnNum;
-};
