1ef21fd0833dd07a33b9cbdc5a30700e101ca60f
KASHIHARAAkira <akira.kashihara@hotmail.com>
2020-07-29 23:04:16 +0900

    Inspect url on index

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,10 @@
 const form = document.getElementById("generateForm");
 
+const options = {
+  mode: "no-cors",
+  method: "GET",
+};
+
 // フォームの submit イベントを乗っ取ります
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -15,8 +20,25 @@ form.addEventListener("submit", (event) => {
     console.log("url is not github repository");
     return;
   }
-  const owner = splitUrl[3];
-  const repo = splitUrl[4];
-  // console.log(`?owner=${owner}&repo=${repo}&autofill=${autoFill}`);
-  window.location.href = `/makereadme?owner=${owner}&repo=${repo}&autofill=${autoFill}`;
+
+  fetch(url, options)
+    .then(function (response) {
+      if (response.ok) {
+        const owner = splitUrl[3];
+        const repo = splitUrl[4];
+        // console.log(`?owner=${owner}&repo=${repo}&autofill=${autoFill}`);
+        window.location.href = `/makereadme?owner=${owner}&repo=${repo}&autofill=${autoFill}`;
+      }
+      throw new Error("Network response was not ok.");
+    })
+    .then(function (myBlob) {
+      var objectURL = URL.createObjectURL(myBlob);
+      myImage.src = objectURL;
+    })
+    .catch(function (error) {
+      console.log(
+        "There has been a problem with your fetch operation: ",
+        error.message
+      );
+    });
 });
