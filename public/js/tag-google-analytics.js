d7cc17234a2e0634db708772e9325e78461990ba
KASHIHARAAkira <akira.kashihara@hotmail.com>
2020-08-22 02:45:49 +0900

    Fix argument of document.write

--- public/js/tag-google-analytics.js
+++ public/js/tag-google-analytics.js
@@ -1,8 +1,5 @@
 document.write(
-  <script
-    async
-    src="https://www.googletagmanager.com/gtag/js?id=UA-117292044-2"
-  ></script>
+  "<script async src='https://www.googletagmanager.com/gtag/js?id=UA-117292044-2'></script>"
 );
 window.dataLayer = window.dataLayer || [];
 function gtag() {
