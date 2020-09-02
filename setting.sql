5d3f2edddbbf562e804bbb54d4985e51409e8a1e
KASHIHARAAkira <akira.kashihara@hotmail.com>
2020-09-02 23:06:44 +0900

    Initial sql setting file.

---
+++ setting.sql
@@ -0,0 +1,19 @@
+create database leadyou;
+create user 'generator'@'localhost' identified by 'Hf%3k@9LfU';
+grant select,update,insert on leadyou.* to 'generator'@'localhost';
+
+create table leadyou.generate
+(
+    ts TIMESTAMP,
+    user varchar
+    (256), repository varchar
+    (256)
+    );
+
+    create table leadyou.uniqueGene
+    (
+        ts TIMESTAMP,
+        user varchar
+        (256), repository varchar
+        (256)
+        );
