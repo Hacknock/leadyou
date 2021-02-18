d37d998be6ad15427308e9860675549a87733830
Kyome22 <kyomesuke@gmail.com>
2021-02-18 23:05:30 +0900

    READMEを効率よく収集して最大９つまで表示するようにした

--- server.js
+++ server.js
@@ -285,7 +285,7 @@ const getList = async (res) => {
     conn = await pool.getConnection();
     await conn.query("use leadyou");
     const records = await conn.query(
-      "select * from uniqueGene order by ts desc limit 24"
+      "select * from uniqueGene order by ts desc limit 9"
     );
     delete records.meta;
     res.json(records);
