6cb88b2cbc1434dee49f7a1a622d5b661237f809
Akira Kashihara <akira.kashihara@hotmail.com>
2022-02-14 17:42:23 +0900

    Improve response to render front-end according to essential cookie.

--- public/js/tag-google-analytics.js
+++ public/js/tag-google-analytics.js
@@ -1,15 +1,15 @@
-enableGA = () => {
-  let sc = document.createElement("script");
-  sc.async = true;
-  sc.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=UA-117292044-2");
-  document.getElementsByTagName("head")[0].appendChild(sc);
-  document.cookie = "cookie = true";
-  window.dataLayer = window.dataLayer || [];
-  function gtag() {
-    dataLayer.push(arguments);
-  }
-  gtag("js", new Date());
+// enableGA = () => {
+//   let sc = document.createElement("script");
+//   sc.async = true;
+//   sc.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=UA-117292044-2");
+//   document.getElementsByTagName("head")[0].appendChild(sc);
+//   document.cookie = "cookie = true";
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag() {
+//     dataLayer.push(arguments);
+//   }
+//   gtag("js", new Date());
 
-  gtag("config", "UA-117292044-2");
+//   gtag("config", "UA-117292044-2");
 
-}
+// }
