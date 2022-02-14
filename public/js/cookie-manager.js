085d227e7d007f75aa5aa3686bc5bf731a285a41
Akira Kashihara <akira.kashihara@hotmail.com>
2022-02-14 17:50:00 +0900

    Fix sentence of agreement of cookies and google font.

--- public/js/cookie-manager.js
+++ public/js/cookie-manager.js
@@ -24,10 +24,8 @@ const enableFont = () => {
 
 document.cookie.split(";").some(item => {
     if (item.trim().indexOf("font=") === 0) {
-        // attentionCookie.style.display = "none";
         if (item.trim().split("=")[1] === "true") enableFont();
     } else if (item.trim().indexOf("cookie=") === 0) {
-        // attentionCookie.style.display = "none";
         if (item.trim().split("=")[1] === "true") enableGA();
     } else {
         //nothing
--- public/js/index.js
+++ public/js/index.js
@@ -41,21 +41,11 @@ acceptFonts.addEventListener("click", () => {
   attentionCookie.style.display = "none";
 });
 
-// const enableFont = () => {
-//   const link = document.createElement("link");
-//   link.setAttribute("href", "https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap");
-//   link.setAttribute("rel", "stylesheet");
-//   document.getElementsByTagName("head")[0].appendChild(link);
-//   document.cookie = "font = true";
-// }
-
 document.cookie.split(";").some(item => {
   if(item.trim().indexOf("font=") === 0) {
     attentionCookie.style.display = "none";
-    // if (item.trim().split("=")[1] === "true") enableFont();
   } else if (item.trim().indexOf("cookie=") === 0){
     attentionCookie.style.display = "none";
-    // if (item.trim().split("=")[1] === "true") enableGA();
   } else {
     //nothing
   }
