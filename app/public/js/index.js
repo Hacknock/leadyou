9af3b10ed3a7a2e1e4baca6f9e3edc092ac1483a
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2022-03-03 01:16:11 +0900

    escapeとatobはdeprecatedなので手法を変更

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,12 +1,12 @@
 /**
  * Copyright 2022 Hacknock
- * 
+ *
  * Licensed under the Apache License, Version 2.0(the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -73,7 +73,7 @@ const getGeneratedReadme = async (owner, repo) => {
           return {
             path: `${owner}/${repo}`,
             branch: json.url.split("?ref=")[1],
-            text: decodeURIComponent(escape(atob(json.content))),
+            text: Buffer.from(json.content, "base64").toString("utf8"),
           };
         } else {
           throw new Error("readme not found");
