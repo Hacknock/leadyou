55c0d998c892b748e5a34e989be6da89b3183468
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2022-03-12 16:58:39 +0900

    remove sudo

--- docker-alias.sh
+++ docker-alias.sh
@@ -1,12 +1,12 @@
 # long version
-alias docker-daemon='sudo docker-compose up --build -d'
-alias docker-daemon-log='sudo docker-compose up --build'
-alias docker-daemon-stop='sudo docker-compose stop'
-alias docker-daemon-down='sudo docker-compose down'
-alias docker-deploy='sudo docker-compose -f docker-compose.yml -f ./docker/production.yml up --build -d'
-alias docker-test='sudo docker-compose -f docker-compose.yml -f ./docker/test.yml up --build --abort-on-container-exit'
-alias docker-ps='sudo docker-compose ps'
-alias docker-rm='sudo docker-compose rm'
+alias docker-daemon='docker-compose up --build -d'
+alias docker-daemon-log='docker-compose up --build'
+alias docker-daemon-stop='docker-compose stop'
+alias docker-daemon-down='docker-compose down'
+alias docker-deploy='docker-compose -f docker-compose.yml -f ./docker/production.yml up --build -d'
+alias docker-test='docker-compose -f docker-compose.yml -f ./docker/test.yml up --build --abort-on-container-exit'
+alias docker-ps='docker-compose ps'
+alias docker-rm='docker-compose rm'
 
 # short version
 alias dd=docker-daemon
