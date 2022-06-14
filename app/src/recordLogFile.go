df5d925a65f9fe1f2f01fea838dff0b52fcde24e
Akira Kashihara <akira.kashihara@hotmail.com>
2022-06-14 18:15:36 +0900

    Complete to implement RecordLogFile.Error() and its testing.

--- app/src/recordLogFile.go
+++ app/src/recordLogFile.go
@@ -2,28 +2,34 @@ package recordLogFile
 
 import (
 	"log"
-	"time"
 	"os"
+	"time"
 )
 
 type RecordLogFile struct {
-	level int
-	path string
-	}
+	level     int
+	path      string
+	file_name string
+}
 
 // The function to record logs to file.
 func (r RecordLogFile) Error(mess string) (rec string, err error) {
 	t := time.Now()
 
+	// Check the existing of directory
+	if _, err := os.Stat(r.path); os.IsNotExist(err) {
+		os.Mkdir(r.path, 0777)
+	}
+
 	// File open or create
-	f, err := os.OpenFile("test.txt", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0600)
+	f, err := os.OpenFile(r.path+"/"+r.file_name, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0600)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
 
 	// Write error line
-	_, er := f.WriteString("[" + t.Format(time.RFC3339) + "][Error] - " + mess)
+	_, er := f.WriteString("[" + t.Format(time.RFC3339) + "][Error] - " + mess + "\n")
 	if er != nil {
 		log.Fatal(er)
 	}
--- app/src/recordLogFile_test.go
+++ app/src/recordLogFile_test.go
@@ -3,38 +3,50 @@ package recordLogFile
 import (
 	"bufio"
 	"os"
+	"strconv"
 	"testing"
 	"time"
 )
 
-func TestRecordLogFile(t *testing.T) {
-	mess := "Hello"
+func TestError(t *testing.T) {
+	// Get the present time
 	tim := time.Now()
-	recordLogFile := RecordLogFile{0, "test/test.txt"}
-	rec, err := recordLogFile.Error(mess)
-	if rec != "["+tim.Format(time.RFC3339)+"][Error] - "+mess || err != nil {
-		t.Fatal("Does not much record or err exists.")
+
+	// Initialize recordLogFile
+	recordLogFile := RecordLogFile{0, "./test", "log.txt"}
+
+	// Make the arguments
+	mess := []string{"Hello", "This is a test"}
+	var expects []string
+	for i := 0; i < len(mess); i++ {
+		expects = append(expects, "["+tim.Format(time.RFC3339)+"][Error] - "+mess[i])
+		rec, err := recordLogFile.Error(mess[i])
+
+		// Check the return
+		if rec != expects[i] || err != nil {
+			t.Fatal("Does not much record or err exists.")
+		}
 	}
 
-	file, er := os.Open("test.txt")
+	// File open to check the written lines by recordLogFile.Error()
+	file, er := os.Open(recordLogFile.path + "/" + recordLogFile.file_name)
 	if er != nil {
 		t.Fatal("Fail to open the file. test.txt")
 	}
 	defer file.Close()
 
+	// Set scanner to read lines on the file
 	scanner := bufio.NewScanner(file)
 
-	var line string
-
+	// Make variable to count lines
+	l := 0
 	for scanner.Scan() {
-		line = scanner.Text()
+		line := scanner.Text()
+		if line != expects[l] {
+			t.Fatal("Not matched line " + strconv.Itoa(l) + ".\n" +
+				"Expected: " + expects[l] +
+				"\nActual: " + line)
+		}
+		l++
 	}
-
-	if erro := scanner.Err(); erro != nil {
-		t.Fatal("Sccaner Error.")
-	}
-	if line != "["+tim.Format(time.RFC3339)+"][Error] - "+mess {
-		t.Fatal("No match written line.")
-	}
-
 }
