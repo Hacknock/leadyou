b51dedd938b58419a27a5071e1a7a2d4d83e9c2b
KASHIHARAAkira <akira.kashihara@hotmail.com>
2022-06-14 14:44:13 +0900

    Make testing provisionally: RecordLogFile.Error()

--- app/src/recordLogFile.go
+++ app/src/recordLogFile.go
@@ -1,6 +1,33 @@
 package recordLogFile
 
+import (
+	"log"
+	"time"
+	"os"
+)
+
+type RecordLogFile struct {
+	level int
+	path string
+	}
+
 // The function to record logs to file.
-func RecordLogFile(mess string) (rec string, err error) {
-	return "[2022-06-13 19:52][Error] - Hello", nil
+func (r RecordLogFile) Error(mess string) (rec string, err error) {
+	t := time.Now()
+
+	// File open or create
+	f, err := os.OpenFile("test.txt", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0600)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	// Write error line
+	_, er := f.WriteString("[" + t.Format(time.RFC3339) + "][Error] - " + mess)
+	if er != nil {
+		log.Fatal(er)
+	}
+
+	log.Println("[" + t.Format(time.RFC3339) + "][Error] - " + mess)
+	return "[" + t.Format(time.RFC3339) + "][Error] - " + mess, nil
 }
--- app/src/recordLogFile_test.go
+++ app/src/recordLogFile_test.go
@@ -2,11 +2,38 @@ package recordLogFile
 
 import (
 	"testing"
+	"time"
+	"os"
+	"bufio"
 )
 
 func TestRecordLogFile(t *testing.T) {
-	rec, err := RecordLogFile("Hello")
-	if rec != "[2022-06-13 19:52][Error] - Hello" || err != nil {
+	mess := "Hello"
+	tim := time.Now()
+	recordLogFile := RecordLogFile{0, "test/test"}
+	rec, err := recordLogFile.Error(mess)
+	if rec != "[" + tim.Format(time.RFC3339) + "][Error] - " + mess || err != nil {
 		t.Fatal("Does not much record or err exists.")
+	}
+
+	file, er := os.Open("test.txt")
+	if er != nil {
+		t.Fatal("Fail to open the file. test.txt")
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var line string
+
+	for scanner.Scan() {
+		line = scanner.Text()
+	}
+
+	if erro:= scanner.Err(); erro != nil {
+		t.Fatal("Sccaner Error.")
+	}
+	if line != "[" + tim.Format(time.RFC3339) + "][Error] - " + mess {
+		t.Fatal("No match written line.")
 	}
 }
