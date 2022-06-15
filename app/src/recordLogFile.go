ee55bd1c9171be1ce8de3557690f0d5825126929
Akira Kashihara <akira.kashihara@hotmail.com>
2022-06-15 10:51:21 +0900

    Make a new common function which is called 'temp' that write line to a file and stdout.

--- app/src/recordLogFile.go
+++ app/src/recordLogFile.go
@@ -12,10 +12,9 @@ type RecordLogFile struct {
 	file_name string
 }
 
-// The function to record logs to file.
-func (r RecordLogFile) Error(mess string) (rec string, err error) {
-	t := time.Now()
-
+func (r RecordLogFile) temp(time_data string, cate string, mess string) (rec string, err error) {
+	// The message line
+	mess_line := "[" + time_data + "][" + cate + "] - " + mess
 	// Check the existing of directory
 	if _, err := os.Stat(r.path); os.IsNotExist(err) {
 		os.Mkdir(r.path, 0777)
@@ -32,13 +31,25 @@ func (r RecordLogFile) Error(mess string) (rec string, err error) {
 	defer f.Close()
 
 	// Write error line
-	_, er := f.WriteString("[" + t.Format(time.RFC3339) + "][Error] - " + mess + "\n")
+	_, er := f.WriteString(mess_line + "\n")
 	if er != nil {
 		log.Fatal(er)
 		return "", er
 	}
 
 	// To output the log to terminal
-	log.Println("[" + t.Format(time.RFC3339) + "][Error] - " + mess)
-	return "[" + t.Format(time.RFC3339) + "][Error] - " + mess, nil
+	log.Println(mess_line)
+	return mess_line, nil
+}
+
+// The function to record logs to file.
+func (r RecordLogFile) Error(mess string) (rec string, err error) {
+	t := time.Now()
+	res, err := r.temp(t.Format(time.RFC3339), "Error", mess)
+	if err != nil {
+		log.Fatal(err)
+		return "", err
+	}
+
+	return res, nil
 }
