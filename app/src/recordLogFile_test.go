a72788524e1be698bcf231915dd1294068fa2ea9
Akira Kashihara <akira.kashihara@hotmail.com>
2022-06-15 13:07:37 +0900

    Add the implementation and testing to Log()

--- app/src/recordLogFile_test.go
+++ app/src/recordLogFile_test.go
@@ -138,3 +138,46 @@ func TestWarn(t *testing.T) {
 		l++
 	}
 }
+
+func TestLog(t *testing.T) {
+	// Get the present time
+	tim := time.Now()
+
+	// Initialize recordLogFile
+	recordLogFile := RecordLogFile{0, "./test/aaa/bbb", "log.txt"}
+
+	// Make the arguments
+	mess := []string{"Good night", "This is a test", "Testing"}
+	var expects []string
+	for i := 0; i < len(mess); i++ {
+		expects = append(expects, "["+tim.Format(time.RFC3339)+"][Log] - "+mess[i])
+		rec, err := recordLogFile.Log(mess[i])
+
+		// Check the return
+		if rec != expects[i] || err != nil {
+			t.Fatal("Does not much record or err exists.")
+		}
+	}
+
+	// File open to check the written lines by recordLogFile.Log()
+	file, er := os.Open(recordLogFile.path + "/" + recordLogFile.file_name)
+	if er != nil {
+		t.Fatal("Fail to open the file. test.txt")
+	}
+	defer file.Close()
+
+	// Set scanner to read lines on the file
+	scanner := bufio.NewScanner(file)
+
+	// Make variable to count lines
+	l := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line != expects[l] {
+			t.Fatal("Not matched line " + strconv.Itoa(l) + ".\n" +
+				"Expected: " + expects[l] +
+				"\nActual: " + line)
+		}
+		l++
+	}
+}
