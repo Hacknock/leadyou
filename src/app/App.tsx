499c4a6c6da3e1a3f85fb68979d0885237be13b6
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2024-02-15 03:17:15 +0900

    OG対応

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
+import { initReactI18next, useTranslation } from "react-i18next";
 import queryString from "query-string";
+import { Helmet } from "react-helmet";
 import { Header, DummyHeader } from "./header/Header";
 import Top from "./top/Top";
 import Editor from "./editor/Editor";
@@ -24,6 +25,7 @@ i18n.use(initReactI18next).init({
 });
 
 export default function App() {
+  const { t } = useTranslation();
   const qs = queryString.parse(window.location.search);
   const scene = ((qs: queryString.ParsedQuery) => {
     if ("scene" in qs && qs.scene === "editor") {
@@ -39,9 +41,19 @@ export default function App() {
   const repo = "repo" in qs && typeof qs.repo === "string" ? qs.repo : null;
   const autoFill = "autofill" in qs && typeof qs.autofill === "string" && qs.autofill === "true";
   const catalog = catalogJSON as CatalogObject;
+  const url = "https://hacknock.github.io/leadyou/";
 
   return (
     <div className="app">
+      <Helmet>
+        <meta name="description" content={t("description") || undefined} />
+        <meta property="og:url" content={url} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={`${t("title")} - ${t("subTitle")}`} />
+        <meta property="og:description" content={t("description") || undefined} />
+        <meta property="og:site_name" content="LEADYOU" />
+        <meta property="og:image" content={`${url}images/og-thumbnail.png`} />
+      </Helmet>
       <Header />
       <DummyHeader />
       {scene === "top" && <Top catalog={catalog} />}
