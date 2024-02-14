3044a44b3584586bb9b2bee809c009788a34314e
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2024-02-14 14:48:44 +0900

    Analyticsを追加

---
+++ dist/js/analytics.js
@@ -0,0 +1,9 @@
+document.write(
+  '<script async src="https://www.googletagmanager.com/gtag/js?id=G-ERBK7TMHV6"></script>'
+);
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "G-ERBK7TMHV6");
